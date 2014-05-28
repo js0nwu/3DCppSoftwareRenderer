a0c9c51159d7824afd76472f100aca920330661c
Jason Wu <jasonwupilly1997@gmail.com>
2014-05-27 20:47:45 -0400

    more work on obj loading

--- thirdpillow/src/mesh.cpp
+++ thirdpillow/src/mesh.cpp
@@ -28,16 +28,26 @@ void mesh::from_obj(char* filename) {
 	char** lines = putils::split_string(model_c, "\n", &l_len);
 	for (int i = 0; i < l_len; i++) {
 		char* line = lines[i];
-		if (strlen(line) > 0 && line[0] == 'v') {
+		if (strlen(line) > 0 && line[0] != '#' && putils::string_contains(line, " ")) {
 			int s_len;
 			char** line_split = putils::split_string(line, " ", &s_len);
 			if (s_len == 4) {
-				float a = atof((const char*)line_split[1]);
-				float b = atof((const char*)line_split[2]);
-				float c = atof((const char*)line_split[3]);
-				vector3* m_vertex_v = new vector3(a, b, c);
-				vertex* m_vertex = new vertex(*m_vertex_v);
-				vertices.push_back(m_vertex);
+				if (strcmp(line_split[0], "v") == 0) {
+					float a = atof((const char*)line_split[1]);
+					float b = atof((const char*)line_split[2]);
+					float c = atof((const char*)line_split[3]);
+					vector3* m_vertex_v = new vector3(a, b, c);
+					vertex* m_vertex = new vertex(*m_vertex_v);
+					vertices.push_back(m_vertex);
+				}
+			}
+			else if (s_len == 3) {
+				if (strcmp(line_split[0], "vt") == 0) {
+					float u = atof((const char*)line_split[1]);
+					float v = atof((const char*)line_split[2]);
+					vector2* m_vertex_uv = new vector2(u, v);
+					uvs.push_back(m_vertex_uv);
+				}
 			}
 		}
 	}
@@ -47,11 +57,35 @@ void mesh::from_obj(char* filename) {
 			int s_len;
 			char** line_split = putils::split_string(line, " ", &s_len);
 			if (s_len == 4) {
-				int a = atoi((const char*)line_split[1]);
-				int b = atoi((const char*)line_split[2]);
-				int c = atoi((const char*)line_split[3]);
-				triangle3* face = new triangle3(vertices[a - 1], vertices[b - 1], vertices[c - 1]);
-				this->faces.push_back(*face);
+				int a = 0, b = 0, c = 0;
+				if (putils::string_contains(line_split[1], "/")) {
+					int f_len;
+					char** face_split = putils::split_string(line_split[1], "/", &f_len);
+					a = atoi((const char*)face_split[0]);
+				}
+				else {
+					a = atoi((const char*)line_split[1]);
+				}
+				if (putils::string_contains(line_split[2], "/")) {
+					int f_len;
+					char** face_split = putils::split_string(line_split[2], "/", &f_len);
+					b = atoi((const char*)face_split[0]);
+				}
+				else {
+					b = atoi((const char*)line_split[2]);
+				}
+				if (putils::string_contains(line_split[3], "/")) {
+					int f_len;
+					char** face_split = putils::split_string(line_split[3], "/", &f_len);
+					c = atoi((const char*)face_split[0]);
+				}
+				else {
+					c = atoi((const char*)line_split[3]);
+				}
+				if (a != 0 && b != 0 && c != 0) {
+					triangle3* face = new triangle3(vertices[a - 1], vertices[b - 1], vertices[c - 1]);
+					this->faces.push_back(*face);
+				}
 			}
 		}
 	}
