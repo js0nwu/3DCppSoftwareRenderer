b161afbd63c66a3f92a17e4450a196351ed62887
Jason Wu <jasonwupilly1997@gmail.com>
2016-01-03 03:13:28 -0500

    get rid of some ide generated vomit

--- thirdpillow/src/vertex.cpp
+++ thirdpillow/src/vertex.cpp
@@ -1,20 +1,13 @@
-/*
- * vertex.cpp
- *
- *  Created on: Apr 13, 2014
- *      Author: jwpilly
- */
-
 #include "vertex.h"
 
-void vertex::set_position(vector3* p) {
-    this->position.set_x(p->get_x());
-    this->position.set_y(p->get_y());
-    this->position.set_z(p->get_z());
+void vertex::set_position(vector3 p) {
+    this->position.set_x(p.get_x());
+    this->position.set_y(p.get_y());
+    this->position.set_z(p.get_z());
 }
 
-vector3* vertex::get_position() {
-    return &this->position;
+vector3 vertex::get_position() {
+    return this->position;
 }
 
 vertex::vertex() {
--- thirdpillow/src/vertex.h
+++ thirdpillow/src/vertex.h
@@ -1,10 +1,3 @@
-/*
- * vertex.h
- *
- *  Created on: Apr 13, 2014
- *      Author: jwpilly
- */
-
 #ifndef VERTEX_H_
 #define VERTEX_H_
 
@@ -13,8 +6,9 @@
 
 class vertex {
 public:
-    void set_position(vector3* p);
-    vector3* get_position();
+    void set_position(vector3 p);
+
+    vector3 get_position();
     vertex();
     vertex(vector3 position);
     virtual ~vertex();
